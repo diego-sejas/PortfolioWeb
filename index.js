cee9b4605608b1114b0f473577007c3fb584c023
Diego A. Sejas <dsejas2015@gmail.com>
2022-08-15 15:56:14 -0300

    ultimas correciones

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ function toggle_onclick($win, $navbar, width){
 /*FUNCTION TYPED */
 var typed = new Typed('#typed' , {
     strings: [
-        'Desarrollador Java | Springboot | API Rest |  ',
-        'Est. Lic. Sistemas - UNLa '
+        ' Desarrollador Java | Springboot | API Rest |   ',
+        ' Est. Lic. Sistemas - UNLa   '
     ],
     typeSpeed: 45,
     backSpeed: 45,
@@ -42,7 +42,8 @@ var typed = new Typed('#typed' , {
 
 var typed_2 = new Typed('#typed_2' , {
     strings: [
-        'con Springboot   '
+        'Desarrollador Java   ',
+        '   con Springboot'
     ],
     typeSpeed: 45,
     backSpeed: 45,
